79849f61eef34f8e0dec66752e12ae522e69feb8
Heisler0 <cheisler72@gmail.com>
2016-12-10 17:58:52 -0500

    Training and Testing is working

--- BioInfo/CancerDataNeuralNet.py
+++ BioInfo/CancerDataNeuralNet.py
@@ -10,18 +10,23 @@ DATA_TRAINING = "training_data.csv"
 DATA_TEST = "test_data.csv"
 
 # Load datasets.
-training_set = tf.contrib.learn.datasets.base.load_csv(filename=DATA_TRAINING,
-                                                       target_dtype=np.int)
-test_set = tf.contrib.learn.datasets.base.load_csv(filename=DATA_TEST,
-                                                   target_dtype=np.int)
+training_set = tf.contrib.learn.datasets.base.load_csv_with_header(filename=DATA_TRAINING,
+                                                                   target_dtype=np.int,
+                                                                   features_dtype=np.float32,
+                                                                   target_column=30)
+
+test_set = tf.contrib.learn.datasets.base.load_csv_with_header(filename=DATA_TEST,
+                                                               target_dtype=np.int,
+                                                               features_dtype=np.float32,
+                                                               target_column=30)
 
 # Specify that all features have real-value data
-feature_columns = [tf.contrib.layers.real_valued_column("", dimension=29)]
+feature_columns = [tf.contrib.layers.real_valued_column("", dimension=30)]
 
 # Build 3 layer DNN with 10, 20, 10 units respectively.
 classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                             hidden_units=[10, 20, 10],
-                                            n_classes=2,
+                                            n_classes=3,
                                             model_dir="/tmp/data_model")
 
 # Fit model
